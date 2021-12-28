c826c4ca37d54b91603faf6d2dddc4ef0d605e27
Grek <nikgrekgr@gmail.com>
2021-12-28 15:49:31 +0300

    homework 5

--- main.cpp
+++ main.cpp
@@ -1,96 +1,140 @@
 #include <iostream>
 #include <clocale>
-#include <string>
+#include <vector>
+
+// 1. Написать функцию которая выводит массив double чисел на экран. Параметры функции это сам массив и его размер.
+// Вызвать эту функцию из main.
+void printDoubleArray(const double* arr, const size_t n) {
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cout << arr[i] << ' ';
+	}
+	std::cout << '\n';
+}
+
+// 2. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+// Написать функцию, заменяющую в принятом массиве 0 на 1, 1 на 0. Выводить на экран массив до изменений и после.
+void revertElements(int* arr, const size_t n) {
+	for (size_t i = 0; i < n; i++)
+	{
+		arr[i] = !arr[i];
+	}
+}
+void printIntArray(const int* arr, const size_t n) {
+	for (size_t i = 0; i < n; i++)
+	{
+		std::cout << arr[i] << ' ';
+	}
+	std::cout << '\n';
+}
+
+// 3. Задать пустой целочисленный массив размером 8. Написать функцию, которая с помощью цикла заполнит его значениями
+// 1 4 7 10 13 16 19 22. Вывести массив на экран.
+void initArrray(int* arr, const size_t n ) {
+	for (size_t i = 0; i < n; i++)
+	{
+		arr[i] = 1 + i * 3;
+	}
+}
+
+// 4. Написать функцию, которой на вход подаётся одномерный массив и число n (может быть положительным, или отрицательным),
+// при этом метод должен циклически сместить все элементы массива на n позиций.
+void moveArrayValues(std::vector<int>& arr, const int n) {
+	size_t size = arr.size();
+	std::vector<int> newArr(size);
+	for (size_t i = 0; i < size; i++)
+	{
+		int posInNewArr;
+		posInNewArr = (static_cast<int>(i) + n) % static_cast<int>(size);
+		if (posInNewArr < 0)
+		{
+			newArr[size + posInNewArr] = arr[i];
+		}
+		else
+		{
+			newArr[posInNewArr] = arr[i];
+		}
+	
+	}
+	arr = newArr;
+}
+void printIntVector(const std::vector<int>& arr) {
+	for (size_t i = 0; i < arr.size(); i++)
+	{
+		std::cout << arr[i] << ' ';
+	}
+	std::cout << '\n';
+}
+
+// 5. Написать функцию, которой передается не пустой одномерный целочисленный массив, она должна вернуть истину если в массиве есть место,
+// в котором сумма левой и правой части массива равны. Примеры: checkBalance([1, 1, 1, || 2, 1]) = true, checkBalance ([2, 1, 1, 2, 1]) = false,
+// checkBalance ([10, || 1, 2, 3, 4]) = true. Абстрактная граница показана символами ||, эти символы в массив не входят.
+bool checkBalance(std::vector<int>& arr) {
+	size_t n = arr.size();
+	if (n < 2) {
+		return false;
+	}
+	int sum1 = arr[0];
+	int sum2 = arr[n-1];
+	size_t left = 0;
+	size_t right = n-1;
+	while (left < right -1) {
+		if (sum1 > sum2) 
+		{
+			sum2 += arr[right - 1];
+			right--;
+		} else if (sum1 < sum2) 
+		{
+			sum1 += arr[left + 1];
+			left++;
+		} else
+		{
+			std::vector<int> subArr = { arr.begin() + left + 1, arr.begin() + right };
+			return checkBalance(subArr);
+		}
+	}
+	if (sum1 == sum2) {
+		return true;
+	}
+	return false;
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    //  1. Написать программу, проверяющую что сумма двух (введенных с клавиатуры) чисел лежит в пределах от 10 до 20 (включительно),
-    // если да – вывести строку "true", в противном случае – "false";
-    {
-        int a = 0;
-        int b = 0;
-        std::cin >> a >> b;
-        int sum = a + b;
-        if (sum >= 10 && sum <= 20) 
-        {
-            std::cout << "true\n";
-        } 
-        else 
-        {
-            std::cout << "false\n";
-        }
-    } 
-    // 2. Написать программу, выводящую на экран строку “true”, если две целочисленные константы, объявленные в её начале либо обе равны
-    // десяти сами по себе, либо их сумма равна десяти. Иначе "false".
-    {
-        int a = 8;
-        int b = 2;
-        if ((a == 10 && b == 10) || (a + b == 10))
-        {
-            std::cout << "true\n";
-        }
-        else
-        {
-            std::cout << "false\n";
-        }
-    }
-    // 3. Написать программу которая выводит на экран список всех нечетных чисел он 1 до 50.
-    // Например: "Your numbers: 1 3 5 7 9 11 13 …". Для решения используйте любой С++ цикл.
-    {
-        std::cout << "Your numbers: ";
-        for (size_t i = 1; i <= 50; i+=2)
-        {
-            std::cout << i << ' ';
-        }
-        std::cout << '\n';
-    }
-    // 4. Со звёздочкой. Написать программу, проверяющую, является ли некоторое число - простым. Простое число — это целое
-    // положительное число, которое делится без остатка только на единицу и себя само.
-    {
-        int n = 0;
-        std::cin >> n;
-        bool isPrime = true;
-        if (n > 0) {
-            for (size_t i = 2; i < n; i++)
-            {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-        }
-        else {
-            isPrime = false;
-        }
-        std::string answer = isPrime ? "Число простое" : "Число не является простым" ;
-        std::cout << answer << '\n';
-    }
-    // 5. Со звёздочкой. Пользователь вводит с клавиатуры число (год): от 1 до 3000. Написать программу, которая определяет
-    // является ли этот год високосным. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-    // Вывести результаты работы программы в консоль.
-    {
-        int year = 0;
-        std::cin >> year;
-        if (year < 1 || year > 3000)
-        {
-            std::cout << "Введен неверный год\n";
-        }
-        else 
-        {
-            bool isLeap = false; 
-            if (year % 4 == 0) {
-                isLeap = true;
-                if (year % 100 == 0) {
-                    isLeap = false;
-                    if (year % 400 == 0) {
-                        isLeap = true;
-                    }
-                }
-            }
-            std::string answer = isLeap ? "Високосный год" : "Невисокосный год";
-            std::cout << answer << '\n';
 
-        }
-    }
+	{
+		double testDoubleArr[] = { 5.6, 9.0, 7.8 , 3.14 };
+		printDoubleArray(testDoubleArr, std::size(testDoubleArr));
+	}
+
+	{
+		int testIntArr[] = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+		size_t intArrSize = std::size(testIntArr);
+		printIntArray(testIntArr, intArrSize);
+		revertElements(testIntArr, intArrSize);
+		printIntArray(testIntArr, intArrSize);
+	}
+
+	{
+		const size_t SIZE = 8;
+		int arr[SIZE];
+		initArrray(arr, SIZE);
+		printIntArray(arr, SIZE);
+	}
+
+	{
+		std::vector<int> arr = { 5,8,9,1,2 };
+		printIntVector(arr);
+		moveArrayValues(arr, 102); // {1,2,5,8,9}
+		printIntVector(arr);
+		moveArrayValues(arr, -12); // {5,8,9,1,2}
+		printIntVector(arr);
+	}
+
+	{
+		std::vector<int> testBalanceArr = { 2, 1, 1, 2, 1 };
+		std::cout << checkBalance(testBalanceArr);
+	}
+
 }
