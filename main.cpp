efeb1036ca7efb9db6856b0affaf25933c8b5e21
Grek <nikgrekgr@gmail.com>
2022-01-11 10:23:14 +0300

    homework 6

--- main.cpp
+++ main.cpp
@@ -1,140 +1,167 @@
 #include <iostream>
 #include <clocale>
-#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <fstream>
 
-// 1. Написать функцию которая выводит массив double чисел на экран. Параметры функции это сам массив и его размер.
-// Вызвать эту функцию из main.
-void printDoubleArray(const double* arr, const size_t n) {
-	for (size_t i = 0; i < n; i++)
-	{
-		std::cout << arr[i] << ' ';
-	}
-	std::cout << '\n';
-}
-
-// 2. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
-// Написать функцию, заменяющую в принятом массиве 0 на 1, 1 на 0. Выводить на экран массив до изменений и после.
-void revertElements(int* arr, const size_t n) {
-	for (size_t i = 0; i < n; i++)
-	{
-		arr[i] = !arr[i];
-	}
-}
-void printIntArray(const int* arr, const size_t n) {
-	for (size_t i = 0; i < n; i++)
-	{
-		std::cout << arr[i] << ' ';
-	}
-	std::cout << '\n';
+void fillArrayWithPowerOfTwo(int* arr, const size_t size) {
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = std::pow(2,i);
+    }
 }
 
-// 3. Задать пустой целочисленный массив размером 8. Написать функцию, которая с помощью цикла заполнит его значениями
-// 1 4 7 10 13 16 19 22. Вывести массив на экран.
-void initArrray(int* arr, const size_t n ) {
-	for (size_t i = 0; i < n; i++)
-	{
-		arr[i] = 1 + i * 3;
-	}
+void fillMatrixWithRandomNumbers(int** arr, const size_t size) {
+    srand(static_cast<unsigned int>(time(0)));
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            arr[i][j] = std::rand() % 100;
+        }
+    }
 }
 
-// 4. Написать функцию, которой на вход подаётся одномерный массив и число n (может быть положительным, или отрицательным),
-// при этом метод должен циклически сместить все элементы массива на n позиций.
-void moveArrayValues(std::vector<int>& arr, const int n) {
-	size_t size = arr.size();
-	std::vector<int> newArr(size);
-	for (size_t i = 0; i < size; i++)
-	{
-		int posInNewArr;
-		posInNewArr = (static_cast<int>(i) + n) % static_cast<int>(size);
-		if (posInNewArr < 0)
-		{
-			newArr[size + posInNewArr] = arr[i];
-		}
-		else
-		{
-			newArr[posInNewArr] = arr[i];
-		}
-	
-	}
-	arr = newArr;
-}
-void printIntVector(const std::vector<int>& arr) {
-	for (size_t i = 0; i < arr.size(); i++)
-	{
-		std::cout << arr[i] << ' ';
-	}
-	std::cout << '\n';
+void printArray(const int* arr, const size_t size) {
+    for (size_t i = 0; i < size ; i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n';
 }
 
-// 5. Написать функцию, которой передается не пустой одномерный целочисленный массив, она должна вернуть истину если в массиве есть место,
-// в котором сумма левой и правой части массива равны. Примеры: checkBalance([1, 1, 1, || 2, 1]) = true, checkBalance ([2, 1, 1, 2, 1]) = false,
-// checkBalance ([10, || 1, 2, 3, 4]) = true. Абстрактная граница показана символами ||, эти символы в массив не входят.
-bool checkBalance(std::vector<int>& arr) {
-	size_t n = arr.size();
-	if (n < 2) {
-		return false;
-	}
-	int sum1 = arr[0];
-	int sum2 = arr[n-1];
-	size_t left = 0;
-	size_t right = n-1;
-	while (left < right -1) {
-		if (sum1 > sum2) 
-		{
-			sum2 += arr[right - 1];
-			right--;
-		} else if (sum1 < sum2) 
-		{
-			sum1 += arr[left + 1];
-			left++;
-		} else
-		{
-			std::vector<int> subArr = { arr.begin() + left + 1, arr.begin() + right };
-			return checkBalance(subArr);
-		}
-	}
-	if (sum1 == sum2) {
-		return true;
-	}
-	return false;
+void printMatrix(int** arr, const size_t size) {
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            std::cout << arr[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-	{
-		double testDoubleArr[] = { 5.6, 9.0, 7.8 , 3.14 };
-		printDoubleArray(testDoubleArr, std::size(testDoubleArr));
-	}
+    // 1. Выделить в памяти динамический одномерный массив типа int. Размер массива запросить у пользователя. Инициализировать его числами
+    // степенями двойки: 1, 2, 4, 8, 16, 32, 64, 128 ... Вывести массив на экран. Не забыть освободить память. =) Разбить программу на функции.
+    {
+        std::cout << "Введите размер массива:\n";
+        size_t n;
+        std::cin >> n;
+        int* pArr = new int[n];
 
-	{
-		int testIntArr[] = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-		size_t intArrSize = std::size(testIntArr);
-		printIntArray(testIntArr, intArrSize);
-		revertElements(testIntArr, intArrSize);
-		printIntArray(testIntArr, intArrSize);
-	}
+        fillArrayWithPowerOfTwo(pArr, n);
 
-	{
-		const size_t SIZE = 8;
-		int arr[SIZE];
-		initArrray(arr, SIZE);
-		printIntArray(arr, SIZE);
-	}
+        std::cout << "Массив степеней двоек:\n";
+        printArray(pArr, n);
 
-	{
-		std::vector<int> arr = { 5,8,9,1,2 };
-		printIntVector(arr);
-		moveArrayValues(arr, 102); // {1,2,5,8,9}
-		printIntVector(arr);
-		moveArrayValues(arr, -12); // {5,8,9,1,2}
-		printIntVector(arr);
-	}
+        delete[] pArr;
+    }
 
-	{
-		std::vector<int> testBalanceArr = { 2, 1, 1, 2, 1 };
-		std::cout << checkBalance(testBalanceArr);
-	}
+    // 2. Динамически выделить матрицу 4х4 типа int. Инициализировать ее псевдослучайными числами через функцию rand. Вывести на экран.
+    // Разбейте вашу программу на функции которые вызываются из main.
+    {
+        const size_t size = 4;
 
+        int** pMatrix = new int*[size];
+        for (size_t i = 0; i < size; i++)
+        {
+            pMatrix[i] = new int[size];
+        }
+
+        fillMatrixWithRandomNumbers(pMatrix, size);
+        std::cout << "Матрица случайных чисел:\n";
+        printMatrix(pMatrix, size);
+
+        for (size_t i = 0; i < size; i++)
+        {
+            delete[] pMatrix[i];
+        }
+        delete[] pMatrix;
+    }
+
+    // 3. Написать программу, которая создаст два текстовых файла (*.txt), примерно по 50-100 символов в каждом
+    // (особого значения не имеет с каким именно содержимым). Имена файлов запросить у польлзователя.
+
+    {
+        std::cout << "Введите имя файла 1:\n";
+        std::string fileName1;
+        std::cin >> fileName1;
+        std::ofstream fout1(fileName1 + ".txt");
+        fout1 << "The class template basic_ofstream implements high-level output operations on file based streams. ";
+        fout1.close();
+
+        std::cout << "Введите имя файла 2:\n";
+        std::string fileName2;
+        std::cin >> fileName2;
+        std::ofstream fout2(fileName2 + ".txt");
+        fout2 << "It interfaces a file-based streambuffer (std::basic_filebuf) with the high-level interface of (std::basic_ostream)";
+        fout2.close();
+    }
+
+    // 4. * Написать функцию, «склеивающую» эти файлы в третий текстовый файл (имя файлов спросить у пользователя).
+    {
+        std::cout << "Введите названия файлов, которые требуется склеить:\n";
+        std::string fileName1, fileName2;
+        std::cin >> fileName1 >> fileName2;
+
+        std::string result = "";
+        std::ifstream fin1(fileName1 + ".txt");
+        while (!fin1.eof()) {
+            std::string buf;
+            std::getline(fin1, buf);
+            result += buf;
+        }
+
+        std::ifstream fin2(fileName2 + ".txt");
+        while (!fin2.eof()) {
+            std::string buf;
+            std::getline(fin2, buf);
+            result += buf;
+        }
+
+        std::cout << "Введите название итогового файла:\n";
+        std::string resultFileName;
+        std::cin >> resultFileName;
+        std::ofstream fout(resultFileName + ".txt", std::ios::app);
+        fout << result;
+    }
+
+    // 5. * Написать программу, которая проверяет присутствует ли указанное пользователем при запуске программы слово в указанном
+    //  пользователем файле (для простоты работаем только с латиницей). Используем функцию find которая есть в строках std::string 
+    {
+        std::cout << "Введите название файла:\n";
+        std::string fileName;
+        std::cin >> fileName;
+
+        std::cout << "Введите искомое слово:\n";
+        std::string word;
+        std::cin >> word;
+
+        std::ifstream fin(fileName + ".txt");
+        bool wordFound = false;
+        while (!fin.eof()) {
+            std::string buf;
+            std::getline(fin, buf);
+            if (buf.find(word) != std::string::npos) {
+                std::cout << buf;
+                wordFound = true;
+                break;
+            }
+        }
+        if (wordFound)
+        {
+            std::cout << "Слово найдено:\n";
+        }
+        else
+        {
+            std::cout << "Слово не найдено:\n";
+        }
+    
+    }
 }
