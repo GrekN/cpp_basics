fee76c5ea673e76c218cd6b8e1d38b5a59625b9b
Grek <nikgrekgr@gmail.com>
2022-02-20 11:43:34 +0300

    homework 8

--- main.cpp
+++ main.cpp
@@ -1,102 +1,320 @@
 #include <iostream>
-#include <fstream>
 #include <clocale>
-#include "mylib.h"
+#include <cstdlib>
+#include <ctime>
+
+enum class Player {
+	User,
+	AI
+};
+
+enum class Cell {
+	EMPTY = '_',
+	CROSS = 'X',
+	NOUGHT = 'O',
+};
+
+enum class Status {
+	IN_PROGRESS,
+	DRAW,
+	CROSS_WIN,
+	NOUGHT_WIN
+};
+
+struct Game {
+	Cell** field;
+	Cell userPiece;
+	Status status;
+	Player currentPlayer;
+};
+
+struct Turn {
+	int x;
+	int y;
+};
+
+Cell getUserPiece() {
+	int randomValue = rand() % 2;
+	if (randomValue == 1) {
+		return Cell::CROSS;
+	}
+	return Cell::NOUGHT;
+}
+
+Game* initGame() {
+	Game* game = new Game();
+	game->field = new Cell * [3];
+	for (size_t i = 0; i < 3; i++)
+	{
+		game->field[i] = new Cell[3];
+	}
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			game->field[i][j] = Cell::EMPTY;
+		}
+	}
+	srand(static_cast<unsigned int>(time(nullptr)));
+	game->userPiece = getUserPiece();
+	game->status = Status::IN_PROGRESS;
+	game->currentPlayer = game->userPiece == Cell::CROSS ? Player::User : Player::AI;
+	return game;
+}
+
+void destroyGame(Game* game) {
+	for (size_t i = 0; i < 3; i++)
+	{
+		delete game->field[i];
+	}
+	delete game->field;
+	delete game;
+}
+
+void printField(const Cell* const* field) { //TODO think about it
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			std::cout << static_cast<char>(field[i][j]) << ' ';
+		}
+		std::cout << '\n';
+	}
+}
+
+Cell** cloneField(const Cell* const* field) {
+	Cell** newField = new Cell * [3];
+	for (size_t i = 0; i < 3; i++)
+	{
+		newField[i] = new Cell[3];
+	}
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			newField[i][j] = field[i][j];
+		}
+	}
+	return newField;
+}
+
+void updateField(Game* game, const int x, const int y, const Cell value) {
+	game->field[x][y] = value;
+}
+
+
+Status getGameStatus(const Cell* const* field) {
+
+	// check rows
+	for (size_t i = 0; i < 3; i++)
+	{
+		if (field[i][0] != Cell::EMPTY && field[i][0] == field[i][1] && field[i][1] == field[i][2]) {
+			if (field[i][0] == Cell::CROSS) {
+				return Status::CROSS_WIN;
+			}
+			else {
+				return Status::NOUGHT_WIN;
+			}
+		}
+	}
+
+	// check columns
+	for (size_t j = 0; j < 3; j++)
+	{
+		if (field[0][j] != Cell::EMPTY && field[0][j] == field[1][j] && field[1][j] == field[2][j]) {
+			if (field[0][j] == Cell::CROSS) {
+				return Status::CROSS_WIN;
+			}
+			else {
+				return Status::NOUGHT_WIN;
+			}
+		}
+	}
+
+	// check diagonals
+	if (field[0][0] != Cell::EMPTY && field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
+		if (field[0][0] == Cell::CROSS) {
+			return Status::CROSS_WIN;
+		}
+		else {
+			return Status::NOUGHT_WIN;
+		}
+	}
+	if (field[2][0] != Cell::EMPTY && field[2][0] == field[1][1] && field[1][1] == field[0][2]) {
+		if (field[2][0] == Cell::CROSS) {
+			return Status::CROSS_WIN;
+		}
+		else {
+			return Status::NOUGHT_WIN;
+		}
+	}
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			if (field[i][j] == Cell::EMPTY) {
+				return Status::IN_PROGRESS;
+			}
+		}
+	}
+
+	return Status::DRAW;
+}
+
+
+void processUserInput(Game* game) {
+	int x, y;
+	do {
+		std::cout << "Сделайте ход:";
+		std::cin >> x >> y;
+		if (x < 1 || x > 3 || y < 1 || y > 3 || game->field[x - 1][y - 1] != Cell::EMPTY) {
+			std::cout << "Неверный номер клетки\n";
+		}
+	} while (x < 1 || x > 3 || y < 1 || y > 3 || game->field[x - 1][y - 1] != Cell::EMPTY);
+
+	updateField(game, x - 1, y - 1, game->userPiece);
+}
+
+int minimax(Cell** field, bool isMaximizer) {
+	Status gameStatus = getGameStatus(field);
+	switch (gameStatus) {
+	case Status::CROSS_WIN:
+		return 10;
+	case Status::NOUGHT_WIN:
+		return -10;
+	case Status::DRAW:
+		return 0;
+	}
+	if (isMaximizer)
+	{
+		int value = -10;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (field[i][j] == Cell::EMPTY) {
+					Cell** newField = cloneField(field);
+					newField[i][j] = Cell::CROSS;
+					int newFieldValue = minimax(newField, false);
+					if (newFieldValue > value) {
+						value = newFieldValue;
+					}
+				}
+			}
+		}
+		return value;
+	}
+	else
+	{
+		int value = 10;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (field[i][j] == Cell::EMPTY) {
+					Cell** newField = cloneField(field);
+					newField[i][j] = Cell::NOUGHT;
+					int newFieldValue = minimax(newField, true);
+					if (newFieldValue < value) {
+						value = newFieldValue;
+					}
+				}
+			}
+		}
+		return value;
+
+	}
+};
+
+Turn getBestTurn(Cell** field, Cell piece) {
+
+	Turn bestTurn;
+	int value;
+
+	if (piece == Cell::CROSS) {
+		value = -10;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (field[i][j] == Cell::EMPTY) {
+					Cell** newField = cloneField(field);
+					newField[i][j] = piece;
+					int newFieldValue = minimax(newField, false);
+					if (newFieldValue > value) {
+						value = newFieldValue;
+						bestTurn = { i, j };
+					}
+				}
+			}
+		}
+	}
+	else {
+		value = 10;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (field[i][j] == Cell::EMPTY) {
+					Cell** newField = cloneField(field);
+					newField[i][j] = piece;
+					int newFieldValue = minimax(newField, true);
+					if (newFieldValue < value) {
+						value = newFieldValue;
+						bestTurn = { i, j };
+					}
+				}
+			}
+		}
+	}
+	return bestTurn;
+}
+
+void processAIInput(Game* game) {
+	Cell aiPeace = game->userPiece == Cell::CROSS ? Cell::NOUGHT : Cell::CROSS;
+	Turn aiTurn = getBestTurn(game->field, aiPeace);
+	updateField(game, aiTurn.x, aiTurn.y, aiPeace);;
+}
 
 int main()
 {
-    setlocale(LC_ALL, "ru_RU.UTF-8");
+	setlocale(LC_ALL, "ru_RU.UTF-8");
 
-    // 1. Создайте проект из 2х cpp файлов и заголовочного (main.cpp, mylib.cpp, mylib.h) во втором модуле mylib объявить 3 функции: 
-    // для инициализации массива (типа float), печати его на экран и подсчета количества отрицательных и положительных элементов. 
-    // Вызывайте эти 3-и функции из main для работы с массивом.
-    // 5.* Сделайте задание 1 добавив свой неймспейс во втором модуле (первое задание тогда можно не делать).
-    {
-        const size_t size = 10;
-        float arr[size];
-        mylib::initArray(arr, size);
-        mylib::printFloatArray(arr, size);
-        mylib::countPositiveAndNegativeNumbers(arr, size);
-    }
+	Game* game = initGame();
+	std::cout << "Вы играете " << static_cast<char>(game->userPiece) << '\n';
+	printField(game->field);
 
-    // 2. Описать макрокоманду (через директиву define), проверяющую, входит ли переданное ей число (введенное с клавиатуры)
-    // в диапазон от нуля (включительно) до переданного ей второго аргумента (исключительно) и возвращает true или false,
-    // вывести на экран «true» или «false».
-    {
-        #define IS_POSITIVE_AND_LOWER_THAN(x,y) (x >= 0 && x < y) ? true : false; 
-       
-        std::cout << "Введите число:\n";
-        int n;
-        std::cin >> n;
-        bool isTruthy = IS_POSITIVE_AND_LOWER_THAN(n, 10);
+	while (game->status == Status::IN_PROGRESS) {
+		if (game->currentPlayer == Player::User)
+		{
+			processUserInput(game);
+			std::cout << "Ваш ход:\n";
+		}
+		else
+		{
+			processAIInput(game);
+			std::cout << "Ход противника:\n";
+		}
+		printField(game->field);
+		game->currentPlayer = game->currentPlayer == Player::User ? Player::AI : Player::User;
+		game->status = getGameStatus(game->field);
+	}
 
-        if (isTruthy) {
-            std::cout << "true";
-        } else {
-            std::cout << "false";
-        }
-        std::cout << '\n';
-    }
+	if ((game->status == Status::CROSS_WIN && game->userPiece == Cell::CROSS) || (game->status == Status::NOUGHT_WIN && game->userPiece == Cell::NOUGHT))
+	{
+		std::cout << "Вы победили !";
+	}
+	else if ((game->status == Status::CROSS_WIN && game->userPiece == Cell::NOUGHT) || (game->status == Status::NOUGHT_WIN && game->userPiece == Cell::CROSS))
+	{
+		std::cout << "Вы проиграли :(";
+	}
+	else
+	{
+		std::cout << "Ничья";
+	}
 
-    // 3. Задайте массив типа int. Пусть его размер задается через директиву препроцессора #define. Инициализируйте его через ввод с клавиатуры.
-    // Напишите для него свою функцию сортировки (например Пузырьком). Реализуйте перестановку элементов как макрокоманду SwapINT(a, b). 
-    // Вызывайте ее из цикла сортировки.
-    {
-        #define ARRSIZE 5
-        int arr[ARRSIZE];
-        for (size_t i = 0; i < ARRSIZE; i++)
-        {
-            std::cout << "Введите " << i << " элемент массива\n";
-            std::cin >> arr[i];
-        }
-
-        #define SwapINT(a,b) int temp = a; a = b; b = temp;
-        bool isSorted = true;
-        for (size_t i = 0; i < ARRSIZE - 1; i++)
-        {
-            for (size_t j = 0; j < ARRSIZE - 1 - i; j++)
-            {
-                if (arr[j] > arr[j + 1]) {
-                    isSorted = false;
-                    SwapINT(arr[j], arr[j + 1]);
-                }
-            }
-            if (isSorted) {
-                break;
-            }
-        }
-        mylib::printIntArray(arr, ARRSIZE);
-    }
-
-    //  4. * Объявить структуру Сотрудник с различными полями. Сделайте для нее побайтовое выравнивание с помощью директивы pragma pack.
-    // Выделите динамически переменную этого типа. Инициализируйте ее. Выведите ее на экран и ее размер с помощью sizeof. Сохраните эту
-    // структуру в текстовый файл.
-
-    {
-        #pragma pack(push,1)
-        struct Employee {
-            char fullName[50];
-            char department[50];
-            bool isManager;
-            unsigned int age;
-            unsigned int grade;
-        };
-        #pragma pack(pop)
-
-        Employee* CTO = new Employee({"Ivanov Ivan Ivanovich", "Executives", true, 50, 10});
-        std::cout << "Full name: " << CTO->fullName << '\n';
-        std::cout << "Department: " << CTO->department << '\n';
-        std::cout << "Age: " << CTO->age << '\n';
-        std::cout << "Grade: " << CTO->grade << '\n';
-        std::cout << "Size of structure: " << sizeof(*CTO) << '\n';
-    
-        std::ofstream fout("result.txt");
-        if (fout.is_open()) {
-            fout << "Full name: " << CTO->fullName << '\n';
-            fout << "Department: " << CTO->department << '\n';
-            fout << "Age: " << CTO->age << '\n';
-            fout << "Grade: " << CTO->grade << '\n';
-            fout.close();
-        }
-    }
+	destroyGame(game);
 }
